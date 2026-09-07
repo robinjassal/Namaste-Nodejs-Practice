72e6148c29f26fd23817dbab6e2589710853916d
Robin <robinjassal601@gmail.com>
2026-09-07 15:00:47 +0530

    name change

--- 04Basics/oldwayserver.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer(function (req, res) {
-  if (req.url === "/api") {
-    res.end("you will get api here");
-  } else res.end("Hello User");
-});
-
-server.listen(4000);
